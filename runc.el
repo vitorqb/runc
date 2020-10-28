adb1f472011fb2b15e17d536d161d33ae4a564da
vitor quintanilha barbosa <vitorqb@gmail.com>
2020-10-28 22:07:14 +0100

    NEW Adds default-args

--- runc.el
+++ runc.el
@@ -26,11 +26,16 @@
 ;;; Code:
 
 
+;; Vars
+(defvar runc-default-runner nil "An instace of `runc-i-runner` used to run the commands.")
+
+
 ;; Base data
 (cl-defstruct runc-runnable
   "Defines a runnable command."
-  (command nil :type 'string :documentation "A string containing the command to run.")
-  (directory nil :type 'string :documentation "The default directory to use for running `command`.")
+  (program nil :type 'string :documentation "A string containing the program to run.")
+  (default-args nil :type 'list :documentation "The default args to be passed to program.")
+  (directory nil :type 'string :documentation "The default directory to use for running `program`.")
   (name nil :type 'string :documentation "A name to identify this runnable."))
 
 
@@ -58,6 +63,7 @@
   "Runs a runnable using a runner."
   (error "Missing implementation"))
 
+
 (defclass runc-simple-runner (runc-i-runner)
   ()
   "Simple runner using *process* and a new filter.")
@@ -65,31 +71,43 @@
 (cl-defmethod runc--runner-run ((runner runc-simple-runner) runnable)
   (let* ((default-directory (runc--default-dir runnable))
          (buffname (runc--buffer-name runnable))
-         (command (runc-runnable-command runnable))
+         (program (runc-runnable-program runnable))
+         (args (runc-runnable-default-args runnable))
          (_ (when (get-buffer buffname)
               (kill-buffer buffname)))
-         (process (start-process buffname buffname command)))
+         (process (apply #'start-process buffname buffname program args)))
     (set-process-filter process #'compilation-filter)
     (with-current-buffer buffname
       (compilation-mode))
     (display-buffer buffname)))
 
-;; API
-(defun runc-run (runnable)
-  "Runs a runnable."
-  (let ((default-directory (runc--default-dir runnable))
-        (compilation-buffer-name-function (-const (runc--buffer-name runnable)))
-        (compile-command (runc-runnable-command runnable)))
+
+(defclass runc-compilation-runner (runc-i-runner)
+  ()
+  "Runner based on Compilation mode, calling `compile` interactively.")
+
+(cl-defmethod runc--runner-run ((runner runc-compilation-runner) runnable)
+  (let* ((default-directory (runc--default-dir runnable))
+         (compilation-buffer-name-function (-const (runc--buffer-name runnable)))
+         (command (runc-runnable-program runnable))
+         (args (--map (shell-quote-argument it) (runc-runnable-default-args runnable)))
+         (compile-command (s-join " " (apply #'list command args))))
     (call-interactively #'compile)))
 
-(cl-defmacro runc-def (defname &key command directory name)
+;; API
+(defun runc-run (runnable &optional runner)
+  "Runs a runnable."
+  (let ((runner* (or runner runc-default-runner (runc-simple-runner))))
+    (runc--runner-run runner* runnable)))
+
+(cl-defmacro runc-def (defname &key program directory name)
   "Defines a runnable and a function to run it."
   (let ((runnable-symbol (-> defname symbol-name (s-concat "-runnable") intern)))
     `(progn
        (setq ,runnable-symbol
-             (make-runc-runnable :command ,command :directory ,directory :name ,name))
+             (make-runc-runnable :program ,program :directory ,directory :name ,name))
        (defun ,defname ()
-         ,(s-concat "Runs the command with name `" name "`.")
+         ,(s-concat "Runs the runnable with name `" name "`.")
          (interactive)
          (runc-run ,runnable-symbol)))))
 
